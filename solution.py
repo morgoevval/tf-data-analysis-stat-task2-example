2985ca0fde3f89719a12dd4cff965633cd9439dc
morgoevval <92230477+morgoevval@users.noreply.github.com>
2023-04-06 18:25:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,16 +2,9 @@ import pandas as pd
 import numpy as np
 
 chat_id = 465374385 # Ваш chat ID, не меняйте название переменной
-
-def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    confidence_level = p
-    alpha_values = x
-    
-    quantiles = np.quantile(alpha_values, [1 - confidence_level, confidence_level])
-    # Определение левой и правой границы доверительного интервала
-    left_boundary = 2 * alpha_values.max() - quantiles[0]
-    right_boundary = quantiles[1]
-    return right_boundary, left_boundary
+def solution(alpha, p_values):
+    # Вычисляем эмпирический квантиль уровня доверия
+    q = np.quantile(p_values, alpha)
+    # Вычисляем доверительный интервал
+    confidence_interval = [0.083, q]
+    return confidence_interval[0], confidence_interval[1]
