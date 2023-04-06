4d9d366f0a1209beaf0a1eb646c87a95e98f0820
morgoevval <92230477+morgoevval@users.noreply.github.com>
2023-04-06 17:49:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import scipy as scipy
 
 from scipy.stats import norm
 
@@ -10,8 +11,13 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 1 - p
+    alpha = p
+    p_values = x
     loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    b_max = np.max(p_values)
+    z = 0
+    if alpha > 0 and alpha < 1:
+        z = np.abs(np.round(scipy.stats.norm.ppf((1 - alpha) / 2), 2))
+    left_bound = b_max
+    right_bound = b_max + z * ((b_max - 0.083) / 2)
+    return left_bound, right_bound
